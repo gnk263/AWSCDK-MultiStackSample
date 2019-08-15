534c23af70d5429699d68120702faa7b9552ebae
gnk263 <gnk263@users.noreply.github.com>
2019-08-15 16:58:42 +0900

    インフラコードを追加

--- lib/awscdk-multi_stack_sample-stack.ts
+++ lib/awscdk-multi_stack_sample-stack.ts
@@ -1,3 +1,6 @@
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { Duration } from '@aws-cdk/core';
 import cdk = require('@aws-cdk/core');
 
 export class AwscdkMultiStackSampleStack extends cdk.Stack {
@@ -5,5 +8,30 @@ export class AwscdkMultiStackSampleStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Lambda
+    const sampleLambda = new lambda.Function(this, 'multi-sample-Lambda', {
+      code: lambda.Code.asset('src/lambda'),
+      handler: 'app.handler',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      timeout: Duration.seconds(3),
+    });
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'multi-sample-api', {
+      restApiName: 'MultiSampleApi'
+    });
+
+    const integration = new apigateway.LambdaIntegration(sampleLambda, {
+      proxy: true,
+    });
+
+    const messageResource = api.root.addResource('message');
+    const idResource = messageResource.addResource('{id}');
+    idResource.addMethod('GET', integration);
   }
 }
+
+const app = new cdk.App();
+new AwscdkMultiStackSampleStack(app, 'AWS-CDK-Multi-Stack');
+app.synth();
