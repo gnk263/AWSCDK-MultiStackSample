12c9d3d3e293be5f18b2035b4022b9e680b8288e
gnk263 <gnk263@users.noreply.github.com>
2019-08-15 17:18:51 +0900

    環境名をスタック名に反映させる

--- bin/awscdk-multi_stack_sample.ts
+++ bin/awscdk-multi_stack_sample.ts
@@ -3,5 +3,7 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { AwscdkMultiStackSampleStack } from '../lib/awscdk-multi_stack_sample-stack';
 
+const targetEnv = process.env.SYSTEM_ENV ? process.env.SYSTEM_ENV : 'dev';
+
 const app = new cdk.App();
-new AwscdkMultiStackSampleStack(app, 'AwscdkMultiStackSampleStack');
+new AwscdkMultiStackSampleStack(app, `AwscdkMultiStackSampleStack-${targetEnv}`);
